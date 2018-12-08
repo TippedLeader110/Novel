0e6663246cc271658b6ad3f1f0a0fadfacaafc7b
TippedLeader110 <bayhaqi101@gmail.com>
2018-12-08 19:50:07 +0700

    profile

--- application/views/user_view/profile.php
+++ application/views/user_view/profile.php
@@ -3,11 +3,38 @@
 		<div class="col-md-4">
 			<div class="container" style="border-width:1px;border-color: black">
 				<div class="row" style="background: #ff8533;height: 40px;"> 
-					<div class="col-md-12"></div>
+					<div class="col-md-12">DESC</div>
 				</div>
 
-				<div class="row">
-					
+				<div class="row" style="background: purple;">
+					<div class="col-md-12">	
+						<div class="row">
+							<div class="col-md-10 offset-md-1" style="background: black;margin-top: 20px">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+								quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+								cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+								proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</div>
+						</div>
+						<div class="row" style="margin-top: 10px;">
+							<div class="col-md-5 offset-md-1" style="background: white;border-style: solid;border-color: black;border-width: 1px;">
+								Last Online
+							</div>
+							<div class="col-md-5" style="background: white;border-style: solid;border-color: black;border-width: 1px;">
+								Value
+							</div>
+						</div>
+						<div class="row" style="margin-top: 5px;">
+							<div class="col-md-5 offset-md-1" style="background: white;border-style: solid;border-color: black;border-width: 1px;">
+								Joined
+							</div>
+							<div class="col-md-5" style="background: white;border-style: solid;border-color: black;border-width: 1px;">
+								Value
+							</div>
+						</div>						
+					</div>
 				</div>
 			</div>
 		</div>
@@ -15,23 +42,38 @@
 		<div class="col-md-4">
 			<div class="container" style="border-width:1px;border-color: black">
 				<div class="row" style="background: #ff8533;height: 40px;"> 
-					<div class="col-md-12"></div>
+					<div class="col-md-12">DESC</div>
 				</div>
 
-				<div class="row">
-					
+				<div class="row" style="background: purple">
+					<div class="col-md-10 offset-md-1" style="background: black">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
 				</div>
+				
 			</div>
 		</div>
 
 		<div class="col-md-4">
-			<div class="container" style="border-width:1px;border-color: black">
+			<div class="container" style="border-width:1px;border-color: black;">
 				<div class="row" style="background: #ff8533;height: 40px;"> 
-					<div class="col-md-12"></div>
+					<div class="col-md-12">DESC</div>
 				</div>
 
-				<div class="row">
-					
+				<div class="row" style="background: purple">
+					<div class="col-md-10 offset-md-1" style="background: black">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
 				</div>
 			</div>
 		</div>		
