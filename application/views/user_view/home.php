306974cb6f26afcfe397d173429bb2692cd7f58b
usokun <alihidayat278@gmail.com>
2018-12-29 16:27:28 +0900

    Update home.php

---
+++ application/views/user_view/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		home
+	</title>
+</head>
+<body>
+ <div class="container">
+  <div class="row">
+  	<div>
+   		<h4>Title</h4>
+    	<hr>
+    </div>    
+    <div class="col-8">
+    	<div class="row">
+    		
+    	</div>
+    	
+    </div>
+    <div class="col-4">
+    	<div style="margin-top: 5px; margin-right: 5px; margin-left: 5px; background-color: #ff8533; padding: 2px 2px 2px 2px; color: white;">
+    		<h5 align="center"><b>Top Novel of The Year</b></h5>
+    	</div>
+    	<div style="margin: 0px 5px 5px 5px; background-color: #fff0e6; padding: 10px 10px 10px 10px;">
+    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    	</div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
