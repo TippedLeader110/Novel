cfabdb15e926887f074f9c1a8ad4d9e77c0167d8
Ihsan <m.ihsanmaulana562@gmail,.com>
2019-01-03 15:08:28 +0700

    Update novel.sql

--- novel.sql
+++ novel.sql
@@ -1,3 +1,3 @@
 CREATE TABLE fav (id int PRIMARY KEY, user varchar(30), tanggal varchar(30), nama_novel varchar(30), wikiid int);
-CREATE TABLE komentar (id int PRIMARY KEY, komentar text, wikiid int);
+CREATE TABLE komentar (id int PRIMARY KEY, komentar text, wikiid int, tanggal varchar(30));
 CREATE TABLE popular (id int PRIMARY KEY, wikiid int, view varchar(100));
