06f2f0726ff8446f05bb5d99c293d0fd605afd82
TippedLeader110 <bayhaqi101@gmail.com>
2019-01-03 20:10:20 +0700

    Update searchpage.php

--- application/views/user_view/searchpage.php
+++ application/views/user_view/searchpage.php
@@ -17,18 +17,18 @@
 		<?php if ($i==0): ?>
 		<div class="row">
 		<?php endif ?>
-			<div class="col-md-3" >
+			<div class="col-md-3" style="background: white">
 				<div class="row">
 					<div class="col-md-12">
-						<div class="container-fluid">
-							<a href="<?php echo base_url() ?>home/deskripsi/<?php echo $key['wikiid']['value'] ?>"><img src="<?php echo $key['c']['value'] ?>" style="max-width: 100px"></a>
+						<div class="container-fluid" style="height: 300px">
+							<a href="<?php echo base_url() ?>home/deskripsi/<?php echo $key['wikiid']['value'] ?>"><img src="<?php echo $key['c']['value'] ?>" style="max-height: 300px"></a>
 						</div>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col-md-12">
-						<div class="container-fluid">
-							<a href="<?php echo base_url() ?>home/deskripsi/<?php echo $key['wikiid']['value'] ?>"><h4><?php echo $key['label']['value'] ?></h4></a>
+						<div class="container-fluid"><center>
+							<a href="<?php echo base_url() ?>home/deskripsi/<?php echo $key['wikiid']['value'] ?>"><h5><?php echo $key['label']['value'] ?></h5></a></center>
 						</div>
 					</div>
 				</div>
