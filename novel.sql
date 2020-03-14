2a7ec6dcb10718fd8523b569897be4cd1dff4116
MuhammadBayhaqiDaulay_171402067 <39187403+TippedLeader110@users.noreply.github.com>
2020-03-14 22:41:16 +0700

    Update novel.sql

--- novel.sql
+++ novel.sql
@@ -1,3 +1,4 @@
 CREATE TABLE fav (id int PRIMARY KEY, user varchar(30), tanggal varchar(30), nama_novel varchar(30), wikiid int);
 CREATE TABLE komentar (id int PRIMARY KEY, komentar text, wikiid int, tanggal varchar(30));
 CREATE TABLE popular (id int PRIMARY KEY, wikiid int, view varchar(100));
+CREATE TABLE `novel`.`users` ( `id_user` INT NOT NULL AUTO_INCREMENT ,  `username` VARCHAR(32) NOT NULL ,  `password` VARCHAR(32) NOT NULL ,  `email` INT NOT NULL ,    PRIMARY KEY  (`id_user`)) ENGINE = InnoDB;
